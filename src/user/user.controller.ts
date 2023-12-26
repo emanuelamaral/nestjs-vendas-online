0a166988d88b15de8d7ec2d9deede5598faa6465
amoz <amoz_c.p.t@hotmail.com>
2023-12-26 15:05:16 -0400

    create post method for user

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+
+    @Post()
+    async createUser(@Body() createUser: CreateUserDTO) {
+        return {
+            ...createUser,
+            password: undefined,
+        }
+    }
+}
